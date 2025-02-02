af6581512fe14092286f9fed1006fa8aa9f3f7ab
ru11snc0 <rensull147@gmail.com>
2025-02-02 10:34:28 +1000

    Initial commit

---
+++ Bastion/BastionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BastionApp.swift
+//  Bastion
+//
+//  Created by Renee Sullivan on 2/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct BastionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BastionTests/BastionTests.swift
@@ -0,0 +1,17 @@
+//
+//  BastionTests.swift
+//  BastionTests
+//
+//  Created by Renee Sullivan on 2/2/2025.
+//
+
+import Testing
+@testable import Bastion
+
+struct BastionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
